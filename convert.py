d95f9bde7c43fe20e0247840ef429ea1201961f7
colobas <mail@gpir.es>
2019-06-17 18:21:35 -0700

    deleted old files

--- convert.py
+++
@@ -1,7 +0,0 @@
-import sys
-import pdftotext
-
-with open(sys.argv[1], "rb") as f:
-    pdf = pdftotext.PDF(f)
-
-print("\n\n".join(pdf))
--- parse.py
+++
@@ -1,9 +0,0 @@
-# %%
-from IPython.core.display import display, HTML
-display(HTML("<style>.container { width:100% !important; }</style>"))
-
-# %%
-import requests
-import pdftotext
-import tempfile
-import re
